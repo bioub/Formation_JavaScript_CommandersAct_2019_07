6befecebd33a72125b19125f04360299da91bce3
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-07-18 15:32:33 +0200

    Jour 1.75

--- API-Web/js/app.js
+++ API-Web/js/app.js
@@ -1,47 +1,104 @@
-window.addEventListener('load', function() {
-  // Le DOM est pret + les images ont chargées
-});
+(function() {
+  // New ES5+ (IE10+)
+  'use strict';
+  
+  var test = 'test';
+}());
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Le DOM est pret
-  var btn = document.querySelector('button');
-  btn.addEventListener('click', function() {
-    btn.innerText++;
-  });
+// Un Module IIFE
+// Immediately Invoked Function Express
+(function () {
+  'use strict';
 
-  // TODOLIST
-  /** @type HTMLFormElement */
-  var form = document.querySelector('.todoform');
+  // Requête AJAX, requête en JS depuis le navigateur
+  // Asynchronous JavaScript And XML
+  function fetchTodos(parent) {
+    // API Historique XMLHttpRequest
+    // var xhr = new XMLHttpRequest();
+    // xhr.addEventListener('load', function() {
+    //   var todos = JSON.parse(xhr.responseText);
+    //   todos.slice(0, 10).forEach(function(todo) {
+    //     addTodo(todo.title, parent);
+    //   });
+    // });
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
+    // xhr.send();
 
-  /** @type HTMLInputElement */
-  var input = document.querySelector('.todoform > input');
+    // API Moderne Fetch
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(function(res) {
+        return res.json();
+      })
+      .then(function(todos) {
+        todos.slice(0, 10).forEach(function(todo) {
+          addTodo(todo.title, parent);
+        });
+      });
+  }
 
-  /** @type HTMLDivElement */
-  var div = document.querySelector('.todolist');
-
-  form.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    // input.focus();
-    // input.setSelectionRange(0, 1);
-    
+  function addTodo(value, parent) {
     var ligne = document.createElement('div');
-    ligne.innerText = input.value;
-
+    ligne.innerText = value;
+  
     var btnMoins = document.createElement('button');
     btnMoins.innerText = '-';
     ligne.appendChild(btnMoins);
-
+  
     // Exercice
     // Ecouter le clic du bouton
     // Au clic du bouton supprimer la balise ligne de la div
     // MDN : https://developer.mozilla.org/fr/docs/Web/API/Node/removeChild
     // https://developer.mozilla.org/fr/docs/Web/API/ChildNode/remove
-    btnMoins.addEventListener('click', function() {
-      div.removeChild(ligne);
+    btnMoins.addEventListener('click', function () {
+      parent.removeChild(ligne);
     });
-
-    div.appendChild(ligne);
+  
+    parent.appendChild(ligne);
+  }
+  
+  window.addEventListener('load', function () {
+    // Le DOM est pret + les images ont chargées
   });
   
-});
+  document.addEventListener('DOMContentLoaded', function () {
+
+    
+
+    // Le DOM est pret
+    var btn = document.querySelector('button');
+    btn.addEventListener('click', function () {
+      btn.innerText++;
+    });
+  
+    // TODOLIST
+    /** @type HTMLFormElement */
+    var form = document.querySelector('.todoform');
+  
+    /** @type HTMLInputElement */
+    var input = document.querySelector('.todoform > input');
+  
+    /** @type HTMLDivElement */
+    var div = document.querySelector('.todolist');
+
+    fetchTodos(div);
+  
+    form.addEventListener('submit', function (event) {
+      event.preventDefault();
+      addTodo(input.value, div);
+    });
+
+    input.addEventListener('input', function() {
+      localStorage.setItem('todoValue', input.value);
+    });
+  
+    // Local/Session Storage
+    var valueFromStorage = localStorage.getItem('todoValue');
+    input.value = valueFromStorage;
+
+    // var localStorage = {
+    //   trackingId: 'X3NK4H52K4BXIHD32HBIX2HBI3'
+    // }
+
+  });
+}());
+
